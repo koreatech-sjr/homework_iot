ac9fd134e4a327e934b8a3886eacbad0e252dc3a
sonjaerock <kadamon2007@gmail.com>
2018-05-01 12:29:21 +0900

    add publish

--- application.py
+++ application.py
@@ -8,16 +8,30 @@ def on_connect_subscribe(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print("Topic: " + msg.topic + " Message: " + str(msg.payload))
+    return (msg.topic, msg.payload)
 
 client = mqtt.Client()
 client.on_connect = on_connect_subscribe
 client.on_message = on_message
 client.connect("192.168.0.24", 1883, 60)
 
+
+
 ### publish
+def on_connect_publish(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+def on_publish(client, userdata, mid):
+    msg_id = mid
+    print("message published")
+
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect_publish
+mqttc.on_publish = on_publish
 
 try:
-    client.loop_forever()
+    while True:
+        t = on_message()
 
 except KeyboardInterrupt:
     print("Finished!")
